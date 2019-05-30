836d070328f2eab3ecc2757f0388ceb0830b7c7e
Jack de Grave <jack.degrave@outlook.com>
2019-05-30 08:34:23 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@ require 'pry'
 def prime?(num)
    prime_check = (1..num).to_a 
    prime_check.each do |number|
-     if #{num} % num == 0 
+     if `#{num}` % num == 0 
      false
 end
 binding.pry
